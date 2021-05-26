be828962fc3bc992b3cd6c940c1f47c6101315bb
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-26 14:46:07 -0300

    added new functions of video 🥇

--- util/covertURL.js
+++ util/covertURL.js
@@ -6,7 +6,7 @@ async function covertURL(url) {
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve) => {
     try {
-      const response = await axios.get(url, { responseType: 'arraybuffer', maxContentLength: 100048576 });
+      const response = await axios.get(url, { responseType: 'arraybuffer', maxContentLength: 10000048576 });
       const data = response.data.toString('base64');
       const mime = getMime(data);
       resolve(data && mime ? new MessageMedia(mime, data) : undefined);
